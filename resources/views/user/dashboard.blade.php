264e6621ed3a5b26c8649e2dd2b8e5b34f7e2fd5
balajivalla <balaji.vallabhapuram@gmail.com>
2024-06-24 23:24:27 +0530

    Sellers pop-up heading

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -188,7 +188,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="sellerrequestModalLongTitle">Sales Request</h5>
+        <h5 class="modal-title" id="sellerrequestModalLongTitle">Sellers Request</h5>
         <button type="button" class="sellerclose" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
