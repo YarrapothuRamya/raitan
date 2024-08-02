cd4ba26e2f94a6c527267ee804702e2644c40c3b
raja2122 <vardhinedirajasekhar@gmail.com>
2024-08-02 11:47:06 +0530

    Updated addmin sidebar abd separated services block

--- resources/views/layouts/adminHeader.blade.php
+++ resources/views/layouts/adminHeader.blade.php
@@ -113,6 +113,30 @@
                 </a>
             </li>
             <li>
+					<a  href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-custom-green-main dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <span class="inline-flex justify-center items-center ml-4"></span>
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2l4-4"></path>
+                        </svg>
+                  <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>E-commerce</span>
+                  <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+					<ul id="dropdown-example" class="hidden py-2 space-y-2">
+						<li>
+							<a href="#"
+								class="flex items-center w-full p-2 text-base font-normal text-white-600 hover:text-white-800  pl-11">Products</a>
+						</li>
+						<li>
+							<a href="#"
+								class="flex items-center w-full p-2 text-base font-normal text-white-600 hover:text-white-800  pl-11">Billing</a>
+						</li>
+						<li>
+							<a href="#"
+								class="flex items-center w-full p-2 text-base font-normal text-white-600 hover:text-white-800  pl-11">Invoice</a>
+						</li>
+					</ul>
+				</li>
+            <li>
                 <a href="{{ url('/raitan_admin/agriculture_labour') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-custom-green-main dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
                     <span class="inline-flex justify-center items-center ml-4">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -177,3 +201,5 @@
     </div>
 </div>
 <!-- ./Sidebar -->
+
+<script src="https://unpkg.com/flowbite@1.3.4/dist/flowbite.js"></script>
--- resources/views/tractors/home.blade.php
+++ resources/views/tractors/home.blade.php
@@ -23,40 +23,7 @@
 
   <div class="section-container">
     <div class="lg:flex lg:space-x-6">
-    <div class="lg:min-w-[384px] lg:mt-0 mt-6">
-          <div class="lg:w-96 bg-white rounded-lg z-10">
-            <div>
-              <!-- <p class="text-sm tracking-[0.1px] mb-3 mt-3 md:mt-0">Other Services</p> -->
-              <div
-              class="w-full p-4 bg-neutral-10 dark:bg-neutral-900 rounded-2xl border border-gray-200 dark:border-gray-700 shadow-t-md border">
-              <!-- Lists --max-h-60-->
-              <div
-                class="w-full md:w-[360px] flex flex-col py-2 rounded-xl overflow-hidden bg-neutral-10 dark:bg-neutral-900  overflow-y-auto">
-                
-                @foreach($services as $service)
-                <!-- lists & image -->
-                <a href="<?php echo env('APP_URL'). '/' .$service->route; ?>">
-                <div
-                  class="min-h-[3.5rem] flex flex-row items-center gap-4 py-2 pl-4 pr-6 cursor-pointer border border-pear/30 shadow-md shadow-pear/30 rounded-lg lg:p-4 p-2">
-                  <div
-                    class="w-14 h-14 flex items-center justify-center title-md font-bold bg-primary-600 text-white dark:bg-primary-200 dark:text-neutral-900">
-                    <img src="{{ asset('service_images').'/'.$service->image }}" alt="media 2" class="w-14 h-14">
-                  </div>
-                  <div class="flex flex-col flex-grow">
-                    <p class="tracking-[.03125em]">{{ $service->service_name }}</p>
-                    <span class="text-sm tracking-[0.25px]">{{ $service->description }}</span>
-                  </div>
-                  <span class="text-[11px] leading-4 tracking-[.045em] font-medium"></span>
-                </div></a>
-                @endforeach
-
-
-              </div>
-
-            </div>
-            </div>
-          </div>
-        </div>
+    @include('layouts.allservice')
 
         <div class="relative w-full">
   <div class="sticky mt-2">
